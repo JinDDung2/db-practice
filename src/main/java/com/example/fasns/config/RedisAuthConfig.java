9ae6572893e7c02491ebc2ffbd00848c9229330d
jinhyuck <gjwlsgur4866@gmail.com>
2023-05-29 17:53:34 +0900

    Refactor : "Redis Auth 인증 Config name 변경"

--- src/main/java/com/example/fasns/config/RedisAuthConfig.java
+++ src/main/java/com/example/fasns/config/RedisAuthConfig.java
@@ -10,25 +10,25 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+public class RedisAuthConfig {
 
     private final String host;
 
     private final int port;
 
-    public RedisConfig(@Value("${spring.redis.host}") final String host,
-                       @Value("${spring.redis.port}")final int port) {
+    public RedisAuthConfig(@Value("${spring.data.redis.auth.host}") final String host,
+                           @Value("${spring.data.redis.auth.port}")final int port) {
         this.host = host;
         this.port = port;
     }
 
     // lettuce
-    @Bean
+    @Bean(name = "redisTokenConnectionFactory")
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
 
-    @Bean
+    @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
